e0e23ee376d5706a2cfbed5730a8c9170ff194bf
Permii <PhongNTSE171371@fpt.edu.vn>
2023-10-28 11:33:34 +0700

    feat: login form

--- Tank-Management/Login.Designer.cs
+++ Tank-Management/Login.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace Tank_Management
-{
-    partial class Login
-    {
+﻿namespace Tank_Management {
+    partial class Login {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,14 +22,114 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            lbUsername = new Label();
+            txtUsername = new TextBox();
+            label1 = new Label();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lbUsername
+            // 
+            lbUsername.AutoSize = true;
+            lbUsername.Location = new Point(272, 143);
+            lbUsername.Name = "lbUsername";
+            lbUsername.Size = new Size(75, 20);
+            lbUsername.TabIndex = 0;
+            lbUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(353, 140);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "username";
+            txtUsername.Size = new Size(142, 27);
+            txtUsername.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(272, 184);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(353, 181);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.PlaceholderText = "password";
+            txtPassword.Size = new Size(142, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.Location = new Point(401, 223);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.Green;
+            label2.Location = new Point(353, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 46);
+            label2.TabIndex = 5;
+            label2.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(77, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(650, 488);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(819, 513);
+            Controls.Add(label2);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(label1);
+            Controls.Add(txtUsername);
+            Controls.Add(lbUsername);
+            Controls.Add(pictureBox1);
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbUsername;
+        private TextBox txtUsername;
+        private Label label1;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label label2;
+        private PictureBox pictureBox1;
     }
 }
--- Tank-Management/Login.cs
+++ Tank-Management/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +10,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tank_Management
-{
-    public partial class Login : Form
-    {
-        public Login()
-        {
+namespace Tank_Management {
+    public partial class Login : Form {
+        public UserRepository userRepository;
+        public RoleRepository roleRepostiry = new RoleRepository();
+        public Login() {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e) {
+            userRepository = new UserRepository();
+            var users = userRepository.GetAll().ToList();
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Find user with below conditions
+            var user = users.Where(u => u.Username == username && u.Password == password).FirstOrDefault();
+
+            if (user != null) {
+                var checkRole = roleRepostiry.GetAll().Where(r => r.Id == user.RoleId).FirstOrDefault();
+                Program.user = user;
+                if (checkRole.Description.Equals("ad")) {
+                    var adminDasboard = new AdminDashboard();
+                    adminDasboard.Show();
+                    this.Hide();
+                }
+
+            } else {
+                MessageBox.Show("Login fail");
+            }
         }
     }
 }
