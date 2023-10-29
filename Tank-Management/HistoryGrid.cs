50001540793666cc8faaa0f08d7393a243e9f4d1
Stayer1231 <phongntse171371@fpt.edu.vn>
2023-10-29 23:20:05 +0700

    feature:
    create view history data view grid

--- Tank-Management/HistoryGrid.cs
+++ Tank-Management/HistoryGrid.cs
@@ -1,20 +1,55 @@
-﻿using System;
+﻿using Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tank_Management
-{
-    public partial class HistoryGrid : Form
-    {
-        public HistoryGrid()
-        {
+namespace Tank_Management {
+    public partial class HistoryGrid : Form {
+        public TankRepository tankRepository = new TankRepository();
+        public UnitRepository unitRepository = new UnitRepository();
+        public LocationRepository locationRepository = new LocationRepository();
+        public HistoryRepository historyRepository = new HistoryRepository();
+
+        private int unitId = 0;
+
+        public void updateGridView() {
+            var histories = historyRepository.GetAll().
+                Include(p => p.FromUnit).
+                Include(p => p.ToUnit).
+                Include(p => p.Tank).
+                ToList();
+
+            if (histories != null) {
+                var listHistory = histories.ToList();
+
+                dgvHistory.DataSource = histories.Select(p => new {
+                    Id = p.Id,
+                    FromUnit = p.FromUnit.Name,
+                    ToUnit = p.ToUnit.Name,
+                    TankId = p.TankId,
+                    Date = p.Date,
+                }).ToList();
+            }
+        }
+
+        public HistoryGrid() {
             InitializeComponent();
+
+            updateGridView();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            UserDashboard userDashboard = new UserDashboard();
+            userDashboard.Show();
+            this.Hide();
         }
     }
 }
