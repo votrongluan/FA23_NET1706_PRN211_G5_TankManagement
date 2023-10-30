36d8f531b18c6e52f7f2b303b6b576afba77b14f
namzuize <namndpse171442@fpt.edu.vn>
2023-10-30 13:58:38 +0700

    feat: ammmo

--- Tank-Management/AmmoGrid.cs
+++ Tank-Management/AmmoGrid.cs
@@ -10,27 +10,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tank_Management {
-    public partial class AmmoGrid : Form {
+namespace Tank_Management
+{
+    public partial class AmmoGrid : Form
+    {
         private AmmoRepository ammoRepository = new AmmoRepository();
         private UnitManagerRepository unitManagerRepository = new UnitManagerRepository();
         private UnitRepository unitRepository = new UnitRepository();
 
         internal record AmmoData(int Id, string Name, int Price, int Weight);
 
-        private void disableCreate() {
+        private void disableCreate()
+        {
             btnCreate.Enabled = false;
             btnDelete.Enabled = true;
             btnUpdate.Enabled = true;
         }
 
-        private void enableCreate() {
+        private void enableCreate()
+        {
             btnCreate.Enabled = true;
             btnDelete.Enabled = false;
             btnUpdate.Enabled = false;
         }
 
-        private void clearTextBox() {
+        private void clearTextBox()
+        {
             txtId.Text = "";
             txtName.Text = "";
             txtPrice.Text = "";
@@ -38,7 +43,8 @@ namespace Tank_Management {
         }
 
         //load Ammo
-        private void LoadAmmo(string search = "") {
+        private void LoadAmmo(string search = "")
+        {
             var ammos = ammoRepository.GetAll()
                 .Select(t => new AmmoData(
                  t.Id,
@@ -48,7 +54,8 @@ namespace Tank_Management {
                  ))
                 .ToList();
 
-            if (!string.IsNullOrEmpty(search)) {
+            if (!string.IsNullOrEmpty(search))
+            {
                 ammos = ammos
                     //seach by name, price, weight
                     .Where(t => t.Name.Contains(search, StringComparison.OrdinalIgnoreCase) || t.Price.ToString().Contains(search, StringComparison.OrdinalIgnoreCase) || t.Weight.ToString().Contains(search, StringComparison.OrdinalIgnoreCase))
@@ -57,19 +64,22 @@ namespace Tank_Management {
 
             dgvAmmo.DataSource = ammos;
         }
-        public AmmoGrid() {
+        public AmmoGrid()
+        {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
 
             LoadAmmo();
         }
-        private void btnBack_Click(object sender, EventArgs e) {
+        private void btnBack_Click(object sender, EventArgs e)
+        {
             var adminDashboard = new AdminDashboard();
             adminDashboard.Show();
             this.Hide();
         }
 
-        private void dgvAmmo_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+        private void dgvAmmo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
             txtId.Text = dgvAmmo.Rows[e.RowIndex].Cells[0].Value.ToString();
             txtName.Text = dgvAmmo.Rows[e.RowIndex].Cells[1].Value.ToString();
             txtPrice.Text = dgvAmmo.Rows[e.RowIndex].Cells[2].Value.ToString();
@@ -78,44 +88,51 @@ namespace Tank_Management {
             disableCreate();
         }
 
-        private void btnCreate_Click(object sender, EventArgs e) {
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
             var name = txtName.Text;
             var price = txtPrice.Text;
             var weight = txtWeight.Text;
 
             //check name is not empty
-            if (string.IsNullOrEmpty(name)) {
+            if (string.IsNullOrEmpty(name))
+            {
                 MessageBox.Show("Name is required");
                 return;
             }
 
             //check price is not empty
-            if (string.IsNullOrEmpty(price)) {
+            if (string.IsNullOrEmpty(price))
+            {
                 MessageBox.Show("Price is required");
                 return;
             }
 
             //check weight is not empty
-            if (string.IsNullOrEmpty(weight)) {
+            if (string.IsNullOrEmpty(weight))
+            {
                 MessageBox.Show("Weight is required");
                 return;
             }
 
             //check price are integer and not negative 
-            if (!int.TryParse(price, out int priceInt) || priceInt < 0) {
+            if (!int.TryParse(price, out int priceInt) || priceInt < 0)
+            {
                 MessageBox.Show("Price must be integer and not negative");
                 return;
             }
 
 
             //check weight are integer and not negative
-            if (!int.TryParse(weight, out int weightInt) || weightInt < 0) {
+            if (!int.TryParse(weight, out int weightInt) || weightInt < 0)
+            {
                 MessageBox.Show("Weight must be integer and not negative");
                 return;
             }
 
             //create new ammo
-            var ammo = new Ammo {
+            var ammo = new Ammo
+            {
                 Name = name,
                 Price = priceInt,
                 Weight = weightInt
@@ -133,32 +150,44 @@ namespace Tank_Management {
             //enable create button
             enableCreate();
 
+            //catch enter button
+            this.AcceptButton = btnCreate;
+
 
         }
-        private void btnDelete_Click(object sender, EventArgs e) {
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
             //load id from textbox
             var id = int.Parse(txtId.Text);
 
             //get ammo by id
             var ammo = ammoRepository.GetAll().Where(a => a.Id == id).FirstOrDefault();
 
-            if (ammo != null) {
+            if (ammo != null)
+            {
                 //delete ammo
-                try {
+                try
+                {
                     ammoRepository.Delete(ammo);
 
                     //reload data
                     LoadAmmo();
                     clearTextBox();
                     enableCreate();
-                } catch (Exception) {
+
+                    //catch enter button
+                    this.AcceptButton = btnCreate;
+                }
+                catch (Exception)
+                {
                     //show error message
                     MessageBox.Show("Can not delete this ammo!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
 
-        private void btnUpdate_Click(object sender, EventArgs e) {
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
             //load data from textbox
             var id = int.Parse(txtId.Text);
             var name = txtName.Text;
@@ -166,41 +195,48 @@ namespace Tank_Management {
             var weight = txtWeight.Text;
 
             //check name is not empty
-            if (string.IsNullOrEmpty(name)) {
+            if (string.IsNullOrEmpty(name))
+            {
                 MessageBox.Show("Name is required");
                 return;
             }
 
             //check price is not empty
-            if (string.IsNullOrEmpty(price)) {
+            if (string.IsNullOrEmpty(price))
+            {
                 MessageBox.Show("Price is required");
                 return;
             }
 
             //check weight is not empty
-            if (string.IsNullOrEmpty(weight)) {
+            if (string.IsNullOrEmpty(weight))
+            {
                 MessageBox.Show("Weight is required");
                 return;
             }
 
             //check price are integer and not negative 
-            if (!int.TryParse(price, out int priceInt) || priceInt < 0) {
+            if (!int.TryParse(price, out int priceInt) || priceInt < 0)
+            {
                 MessageBox.Show("Price must be integer and not negative");
                 return;
             }
 
 
             //check weight are integer and not negative
-            if (!int.TryParse(weight, out int weightInt) || weightInt < 0) {
+            if (!int.TryParse(weight, out int weightInt) || weightInt < 0)
+            {
                 MessageBox.Show("Weight must be integer and not negative");
                 return;
             }
 
             //get the ammo by id
             var ammo = ammoRepository.GetAll().Where(a => a.Id == id).FirstOrDefault();
-            if (ammo != null) {
+            if (ammo != null)
+            {
                 //update ammo
-                try {
+                try
+                {
                     ammo.Name = name;
                     ammo.Price = priceInt;
                     ammo.Weight = weightInt;
@@ -211,25 +247,33 @@ namespace Tank_Management {
                     LoadAmmo();
                     clearTextBox();
                     enableCreate();
-                } catch (Exception) {
+
+                    //catch enter button
+                    this.AcceptButton = btnCreate;
+                }
+                catch (Exception)
+                {
                     //show error message
                     MessageBox.Show("Can not update this ammo!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
 
-        private void btnResetAmmoDgv_Click(object sender, EventArgs e) {
+        private void btnResetAmmoDgv_Click(object sender, EventArgs e)
+        {
             LoadAmmo();
             clearTextBox();
             enableCreate();
         }
 
-        private void btnSearch_Click(object sender, EventArgs e) {
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
             var search = txtSearch.Text;
             LoadAmmo(search);
         }
 
-        private void btnReset_Click(object sender, EventArgs e) {
+        private void btnReset_Click(object sender, EventArgs e)
+        {
             clearTextBox();
             enableCreate();
         }
@@ -243,5 +287,6 @@ namespace Tank_Management {
 
             disableCreate();
         }
+
     }
 }
